7071b1451048e984a6924358c66dfc075a968139
Paul Park <paulpark330@hotmail.com>
2021-06-18 17:38:19 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,13 @@
+0 - 9
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (let i = 0; i < 10; i++) {
+  x = i * 2;
+  console.log(x);
+}
+
+for (let i = 100; i > 0; i--) {
+  console.log('Time till explosion', i);
+}
